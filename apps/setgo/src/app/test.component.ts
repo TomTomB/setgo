bb754dcb07ca200993b5030bab2b4a31b6ce0459
Tom-Rune Bornholdt <tr.bornholdt@hotmail.de>
2021-06-16 23:29:24 +0200

    feat: 🎸 add sentry

---
+++ apps/setgo/src/app/test.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'setgo-test',
+  template: `<p>Test</p>`,
+  styles: [``],
+})
+export class TestComponent {}
--- apps/setgo/src/main.ts
+++ apps/setgo/src/main.ts
@@ -2,6 +2,20 @@ import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+import * as Sentry from '@sentry/angular';
+import { Integrations } from '@sentry/tracing';
+
+Sentry.init({
+  dsn: 'https://8be6fdd90748407d8fc6c6c881ae2793@o512127.ingest.sentry.io/5820485',
+  integrations: [
+    new Integrations.BrowserTracing({
+      tracingOrigins: ['localhost'],
+      routingInstrumentation: Sentry.routingInstrumentation,
+    }),
+  ],
+  enabled: environment.production,
+  tracesSampleRate: 1.0,
+});
 
 if (environment.production) {
   enableProdMode();
