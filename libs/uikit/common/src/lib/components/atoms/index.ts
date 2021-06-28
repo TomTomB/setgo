1fd659354094cb4bcbec7da2646e5a86007681e9
Tom-Rune Bornholdt <tr.bornholdt@hotmail.de>
2021-06-29 00:12:50 +0200

    feat: 🎸 add uikit common lib and stylings

---
+++ libs/uikit/common/src/lib/components/atoms/index.ts
@@ -0,0 +1,5 @@
+import { CardComponent } from './card/card.component';
+
+export * from './card/card.component';
+
+export const COMPONENTS = [CardComponent] as const;
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,44 @@ module.exports = {
   purge: ['./apps/**/*.html', './libs/**/*.html'],
   darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: '#42ced5',
+          light: '#7fffff',
+          dark: '#009ca4',
+        },
+        secondary: {
+          DEFAULT: '#e91e6',
+          light: '#ff6090',
+          dark: '#b0003a',
+        },
+        error: {
+          light: '#b00020',
+          dark: '#cf6679',
+        },
+        background: {
+          light: '#fafafa',
+          dark: '#303030',
+        },
+        toolbar: {
+          light: '#f5f5f5',
+          dark: '#212121',
+        },
+        card: {
+          light: '#fff',
+          dark: '#424242',
+        },
+        footer: {
+          light: '#ececec',
+          dark: '#2b2b2b',
+        },
+        chip: {
+          light: '#ececec',
+          dark: '#5a5a5a',
+        },
+      },
+    },
   },
   variants: {
     extend: {},
