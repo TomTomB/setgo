f2289704a2748f74e4ea188140926267e559c473
Tom-Rune Bornholdt <tr.bornholdt@hotmail.de>
2021-06-29 00:39:23 +0200

    fix: 🐛 update deps and build

--- apps/setgo/src/app/app.component.ts
+++ apps/setgo/src/app/app.component.ts
@@ -1,11 +1,15 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  ViewEncapsulation,
+} from '@angular/core';
 import { environment } from '@setgo/env';
 
 @Component({
   selector: 'setgo-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
+  encapsulation: ViewEncapsulation.None,
 })
 export class AppComponent {
   version = environment.version;
--- libs/uikit/common/src/index.ts
+++ libs/uikit/common/src/index.ts
@@ -1,1 +1,2 @@
 export * from './lib/uikit-common.module';
+export * from './lib/components/atoms';
