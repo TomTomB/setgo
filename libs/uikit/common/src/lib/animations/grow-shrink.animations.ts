df2134e2c0aa3fddc21c3f52f83bc5b52e50fa24
Tom-Rune Bornholdt <tr.bornholdt@hotmail.de>
2021-07-03 23:32:28 +0200

    fix: 🐛 animation

--- libs/uikit/common/src/lib/animations/grow-shrink.animations.ts
+++ libs/uikit/common/src/lib/animations/grow-shrink.animations.ts
@@ -5,14 +5,14 @@ export const growShrink = trigger('growShrink', [
     style({
       opacity: 0,
       height: '0px',
-      transform: 'translateY(-20px)',
+      transform: 'scaleY(0)',
     }),
     animate(
       '150ms cubic-bezier(0.4, 0, 0.2, 1)',
       style({
         opacity: 1,
         height: '*',
-        transform: 'translateY(0)',
+        transform: 'scaleY(1)',
       }),
     ),
   ]),
@@ -22,7 +22,7 @@ export const growShrink = trigger('growShrink', [
       style({
         opacity: 0,
         height: '0px',
-        transform: 'translateY(-20px)',
+        transform: 'scaleY(0)',
       }),
     ),
   ]),
