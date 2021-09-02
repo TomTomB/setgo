d8b52b70411043e4c653560c0ec1bbc918afca9b
Tom-Rune Bornholdt <tr.bornholdt@hotmail.de>
2021-09-03 00:57:03 +0200

    fix: 🐛 uikit common npm module name

--- libs/domain/notifications/src/index.ts
+++ libs/domain/notifications/src/index.ts
@@ -1,8 +1,11 @@
 export * from './lib/domain-notifications.module';
 
-export * from './lib/components';
 export * from './lib/constants';
 export * from './lib/mocks';
 export * from './lib/services';
 export * from './lib/types';
 export * from './lib/utils';
+
+export { NotificationComponent } from './lib/components/atoms';
+
+export { NotificationGroupComponent } from './lib/components/molecules';
