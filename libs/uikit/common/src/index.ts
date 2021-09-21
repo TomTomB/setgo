3aa6bb67520a8c008aa711ca10d58ec316821cac
Tom-Rune Bornholdt <tr.bornholdt@hotmail.de>
2021-09-22 00:47:30 +0200

    refactor: 💡 add missing exports

--- libs/uikit/common/src/index.ts
+++ libs/uikit/common/src/index.ts
@@ -4,3 +4,10 @@ export * as Animations from './lib/animations';
 export { CardComponent, SpinnerComponent } from './lib/components/atoms';
 
 export { ButtonCTAComponent } from './lib/components/molecules';
+
+export * from './lib/components/atoms/menu/menu';
+export * from './lib/components/atoms/menu/menu-content';
+export * from './lib/components/atoms/menu/menu-item';
+export * from './lib/components/atoms/menu/menu-panel';
+export * from './lib/components/atoms/menu/menu-trigger';
+export * from './lib/components/atoms/menu/menu.module';
