b2b8fb8ba713215ecbf375565901b8bf13f1bb47
Tom-Rune Bornholdt <tr.bornholdt@hotmail.de>
2021-09-23 23:20:47 +0200

    fix: 🐛 attribute component styling

--- apps/setgo/src/app/app.component.ts
+++ apps/setgo/src/app/app.component.ts
@@ -8,31 +8,18 @@ import {
   ViewEncapsulation,
 } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { IconCollection } from '@setgo/uikit/core';
 import { MappedEntityState } from '@tomtomb/ngrx-toolkit';
-import {
-  NotificationGroup,
-  NotificationsFacade,
-} from '@setgo/store/notifications';
-import { Observable } from 'rxjs';
+import { NotificationsFacade } from '@setgo/store/notifications';
 import { ServiceWorkerFacade } from '@setgo/store/service-worker';
 import { TextFieldComponent, ValidatorsExtra } from '@setgo/uikit/forms';
-import { UiShellFacade } from '@setgo/store/ui/shell';
-import { UiTriggerAction, UpdateAvailableEventWithData } from '@setgo/types';
 import { environment } from '@setgo/env';
-import iconOutlineNotifications from '@iconify/icons-ic/outline-notifications';
-import iconOutlineSettings from '@iconify/icons-ic/outline-settings';
 
 @Component({
   selector: 'setgo-root',
   templateUrl: './app.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
   encapsulation: ViewEncapsulation.None,
-  animations: [
-    Animations.growShrink,
-    Animations.scaleOvershoot,
-    Animations.awaitInner,
-  ],
+  animations: [Animations.growShrink],
 })
 export class AppComponent implements OnInit {
   @ViewChild(TextFieldComponent)
@@ -48,18 +35,6 @@ export class AppComponent implements OnInit {
     email: new FormControl(null, [ValidatorsExtra.email, Validators.required]),
   });
 
-  themeCtrl = new FormControl(null);
-
-  availableUpdate$!: Observable<UpdateAvailableEventWithData | null>;
-
-  notificationShadeVisibility$!: Observable<UiTriggerAction>;
-
-  icons: IconCollection = {
-    iconOutlineNotifications,
-    iconOutlineSettings,
-  };
-  notifications$!: Observable<NotificationGroup[]>;
-
   get emailControl() {
     return this.emailForm.controls.email as FormControl;
   }
@@ -67,23 +42,11 @@ export class AppComponent implements OnInit {
   constructor(
     private _authFacade: AuthFacade,
     private _serviceWorkerFacade: ServiceWorkerFacade,
-    private _uiShellFacade: UiShellFacade,
     private _notificationsFacade: NotificationsFacade,
   ) {}
 
   ngOnInit(): void {
     this._serviceWorkerFacade.startPolling();
-
-    this.notifications$ = this._notificationsFacade.notifications$;
-
-    this.notificationShadeVisibility$ =
-      this._uiShellFacade.notificationShadeVisibility$;
-
-    const currentHardTheme = window.localStorage.getItem('theme');
-
-    this.themeCtrl.setValue(currentHardTheme ?? 'system');
-
-    this.themeCtrl.valueChanges.subscribe((v) => this.setTheme(v));
 
     this._serviceWorkerFacade.availableUpdate$.subscribe((v) => {
       if (v) {
@@ -96,14 +59,6 @@ export class AppComponent implements OnInit {
     });
   }
 
-  setNotificationShadeVisibility(uiAction: UiTriggerAction) {
-    this._uiShellFacade.dispatchSetNotificationShadeVisibility(uiAction);
-  }
-
-  update() {
-    this._serviceWorkerFacade.dispatchActivateUpdate();
-  }
-
   checkEmail() {
     if (this.emailForm.invalid) {
       this.emailFieldRef?.inputComponentRef?.nativeInputRef?.nativeElement.focus();
@@ -114,43 +69,5 @@ export class AppComponent implements OnInit {
       this._authFacade.fetchSignInMethodsForEmail({
         body: { email: this.emailForm.value.email },
       });
-  }
-
-  setTheme(theme: 'light' | 'dark' | 'system') {
-    if (theme === 'system') {
-      window.localStorage.removeItem('theme');
-
-      if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
-        document.documentElement.classList.add('dark');
-      } else {
-        document.documentElement.classList.remove('dark');
-      }
-
-      window
-        .matchMedia('(prefers-color-scheme: dark)')
-        .addEventListener('change', this._themeEventHandler);
-    } else {
-      window.localStorage.setItem('theme', theme);
-
-      if (theme === 'dark') {
-        document.documentElement.classList.add('dark');
-      } else {
-        document.documentElement.classList.remove('dark');
-      }
-    }
-  }
-
-  private _themeEventHandler(event: MediaQueryListEvent) {
-    const currentHardTheme = window.localStorage.getItem('theme');
-
-    if (currentHardTheme) {
-      return;
-    }
-
-    if (event.matches) {
-      document.documentElement.classList.add('dark');
-    } else {
-      document.documentElement.classList.remove('dark');
-    }
   }
 }
--- libs/uikit/common/src/lib/components/molecules/button-cta/button-cta.component.ts
+++ libs/uikit/common/src/lib/components/molecules/button-cta/button-cta.component.ts
@@ -1,8 +1,11 @@
 import {
   ChangeDetectionStrategy,
   Component,
+  ElementRef,
   HostBinding,
   Input,
+  OnChanges,
+  SimpleChanges,
   ViewEncapsulation,
 } from '@angular/core';
 
@@ -13,7 +16,7 @@ import {
   changeDetection: ChangeDetectionStrategy.OnPush,
   encapsulation: ViewEncapsulation.None,
 })
-export class ButtonCTAComponent {
+export class ButtonCTAComponent implements OnChanges {
   @Input()
   isLoading?: boolean | null;
 
@@ -29,5 +32,112 @@ export class ButtonCTAComponent {
   @HostBinding('attr.aria-disabled')
   get value() {
     return this.isLoading || this.isDisabled;
+  }
+
+  @HostBinding('class')
+  classes = `
+    block
+    group
+    after:transition-opacity
+    after:absolute
+    after:opacity-0
+    after:top-0
+    after:left-0
+    after:w-full
+    dark:ring-white
+    ring-black
+    after:h-full
+    after:bg-gradient-to-r
+    after:from-brand-primary-light-dark
+    after:to-brand-primary-light
+    dark:after:from-brand-primary-dark-dark dark:after:to-brand-primary-dark
+    active:after:opacity-100
+    hover:before:opacity-100
+    focus-visible:before:opacity-100
+    outline-none
+    focus-visible:ring-2
+    transition
+    tracking-wider
+    uppercase
+    before:transition-opacity
+    overflow-hidden
+    font-bold
+    relative
+    before:absolute
+    before:opacity-0
+    before:top-0
+    before:left-0
+    before:w-full
+    before:h-full
+    before:bg-gradient-to-r
+    before:from-brand-primary-light-dark
+    before:via-brand-primary-light
+    before:to-brand-primary-light
+    dark:before:from-brand-primary-dark-dark
+    dark:before:via-brand-primary-dark
+    dark:before:to-brand-primary-dark-light
+    bg-gradient-to-r
+    from-brand-primary-light-dark
+    via-brand-primary-light
+    to-brand-primary-light-dark
+    dark:from-brand-primary-dark-dark
+    dark:via-brand-primary-dark
+    dark:to-brand-primary-dark-dark
+    w-full
+    py-4
+    px-3
+  `;
+
+  constructor(private _elementRef: ElementRef<HTMLDivElement>) {}
+
+  ngOnChanges(changes: SimpleChanges): void {
+    this._updateClasses(changes);
+  }
+
+  private _updateClasses(changes: SimpleChanges) {
+    const disabledOrLoadingClasses =
+      'after:!opacity-0 before:!opacity-0 cursor-default';
+
+    if (changes.isLoading?.currentValue || changes.isDisabled?.currentValue) {
+      this._elementRef.nativeElement.classList.add(
+        ...disabledOrLoadingClasses.split(' '),
+      );
+    } else {
+      this._elementRef.nativeElement.classList.remove(
+        ...disabledOrLoadingClasses.split(' '),
+      );
+    }
+
+    const roundedXl = 'rounded-xl';
+    const roundedTXl = 'rounded-t-xl';
+    const roundedBXl = 'rounded-b-xl';
+
+    const roundedChange = changes.rounded?.currentValue;
+
+    if (roundedChange) {
+      switch (roundedChange) {
+        case 'both':
+          this._elementRef.nativeElement.classList.add(roundedXl);
+          this._elementRef.nativeElement.classList.remove(
+            roundedTXl,
+            roundedBXl,
+          );
+          break;
+        case 'top':
+          this._elementRef.nativeElement.classList.add(roundedTXl);
+          this._elementRef.nativeElement.classList.remove(
+            roundedXl,
+            roundedBXl,
+          );
+          break;
+        case 'bottom':
+          this._elementRef.nativeElement.classList.add(roundedBXl);
+          this._elementRef.nativeElement.classList.remove(
+            roundedXl,
+            roundedTXl,
+          );
+          break;
+      }
+    }
   }
 }
