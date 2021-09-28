12eee08c8f55f7b4f943104d80dc8cce9aac2165
Tom-Rune Bornholdt <tr.bornholdt@hotmail.de>
2021-09-29 00:25:04 +0200

    test: 💍 fix

--- libs/domain/notifications/src/lib/components/molecules/float-notification/float-notification.component.spec.ts
+++ libs/domain/notifications/src/lib/components/molecules/float-notification/float-notification.component.spec.ts
@@ -1,5 +1,10 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {
+  NOTIFICATION_GROUP_MOCK,
+  NOTIFICATION_MOCK,
+} from '@setgo/store/notifications';
 import { NotificationComponent } from '../../atoms';
+import { UikitCoreModule } from '@setgo/uikit/core';
 
 import { FloatNotificationComponent } from './float-notification.component';
 
@@ -10,12 +15,17 @@ describe('FloatNotificationComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [FloatNotificationComponent, NotificationComponent],
+      imports: [UikitCoreModule],
     }).compileComponents();
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(FloatNotificationComponent);
     component = fixture.componentInstance;
+    component.notificationMessage = {
+      ...NOTIFICATION_MOCK,
+      group: NOTIFICATION_GROUP_MOCK,
+    };
     fixture.detectChanges();
   });
 
